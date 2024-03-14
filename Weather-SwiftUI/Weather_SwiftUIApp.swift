991c1552828ba9e6a12e26e5f201748668dd9a5b
Sadia Tasnim <tasnimsadia259@gmail.com>
2024-03-14 12:02:09 +0600

    Initial Commit

---
+++ Weather-SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather-SwiftUI
+//
+//  Created by Sadia on 14/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
